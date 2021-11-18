3c1161f1d5d8f1873858a6f92cb050afa4a5b6af
Samu698 <samu2altervista@gmail.com>
2021-11-18 16:12:04 +0100

    Functions for running youtube-dl by passing a YtResponse
    This commit add two functions getDownloadInfo and fromDownloadInfo,
    allowing to collect information about a video and then downloading the
    video, without having to download the page twice.
    This is possible using the dumpSingleJson and the loadInfoJson flags
    from youtube-dl

--- src/index.js
+++ src/index.js
@@ -11,9 +11,22 @@ const isJSON = (str = '') => str.startsWith('{')
 const parse = ({ stdout }) => (isJSON(stdout) ? JSON.parse(stdout) : stdout)
 
 const create = binaryPath => {
-  const fn = (url, flags, opts) => fn.raw(url, flags, opts).then(parse)
-  fn.raw = (url, flags, opts) => execa(binaryPath, args(url, flags), opts)
-  return fn
+    const fn = (url, flags, opts) => fn.raw(url, flags, opts).then(parse)
+    fn.raw = (url, flags, opts) => execa(binaryPath, args(url, flags), opts)
+    fn.getDownloadInfo = (url, flags, opts) => {
+        flags ||= {}
+        flags.dumpSingleJson = true;
+        return fn.raw(url, flags, opts).then(parse)
+    }
+    fn.fromDownloadInfo = (info, flags, opts) => {
+        flags ||= {}
+        flags.loadInfoJson = '-'
+        const process = execa(binaryPath, dargs(flags, { useEquals: false }), opts)
+        process.stdin.write(JSON.stringify(info));
+        process.stdin.end();
+        return process.then(parse);
+    }
+    return fn
 }
 
 module.exports = create(require('./constants').YOUTUBE_DL_PATH)
