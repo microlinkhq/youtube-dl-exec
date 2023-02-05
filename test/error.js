a630451a4282f6ed527db9140676f563e97ad284
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-02-05 11:06:05 +0100

    feat: normalize errors

---
+++ test/error.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const test = require('ava')
+
+const youtubedl = require('..')
+
+test('unsupported URLs', async t => {
+  t.plan(4)
+  const url = 'https://www.apple.com/homepod'
+  try {
+    await youtubedl(url, { dumpSingleJson: true, noWarnings: true })
+  } catch (error) {
+    t.is(
+      error.message,
+      'ERROR: Unsupported URL: https://www.apple.com/homepod/'
+    )
+    t.true(error instanceof Error)
+    t.truthy(error.command)
+    t.truthy(error.exitCode)
+  }
+})
+
+test('video unavailable', async t => {
+  t.plan(4)
+  const url = 'https://www.youtube.com/watch?v=x8erEF_1POY'
+  try {
+    await youtubedl(url, { dumpSingleJson: true })
+  } catch (error) {
+    t.is(
+      error.message,
+      'ERROR: [youtube] x8erEF_1POY: Video unavailable. The uploader has not made this video available in your country'
+    )
+    t.true(error instanceof Error)
+    t.truthy(error.command)
+    t.truthy(error.exitCode)
+  }
+})
