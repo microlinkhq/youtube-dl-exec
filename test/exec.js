cc62001784b9ee8293f41d2ded44eac1b2d387b4
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-12-30 12:26:20 +0100

    build: update dependencies

--- test/exec.js
+++ test/exec.js
@@ -4,7 +4,7 @@ const test = require('ava')
 
 const youtubedl = require('../src')
 
-test('acess to subprocess object', async t => {
+test('access to subprocess object', async t => {
   const subprocess = youtubedl.exec(
     'https://www.youtube.com/watch?v=2Z4m4lnjxkY',
     {
