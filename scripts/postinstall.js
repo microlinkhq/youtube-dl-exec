4ed989e7a479f396ac63583a9398d5e97b364e5f
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-04-12 11:06:14 +0200

    refactor: use debug-logfmt

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,29 +1,19 @@
 'use strict'
 
-const { tmpdir } = require('node:os')
-const { Readable } = require('node:stream')
+const debug = require('debug-logfmt')('youtube-dl-exec:install')
+const { mkdir, chmod } = require('node:fs/promises')
 const { pipeline } = require('node:stream/promises')
-const { randomBytes } = require('node:crypto')
 const { createWriteStream } = require('node:fs')
-const { mkdir, chmod, rename } = require('node:fs/promises')
+const { Readable } = require('node:stream')
 
 const mkdirp = filepath => mkdir(filepath, { recursive: true })
 
-const log = (...args) => console.log('[youtube-dl-exec]', ...args)
-
-const get = async url => {
-  const response = await fetch(url, {
+const httpGet = async url =>
+  fetch(url, {
     headers: {
       'user-agent': 'microlinkhq/youtube-dl-exec'
     }
   })
-
-  return {
-    headers: response.headers,
-    stream: () => Readable.fromWeb(response.body),
-    json: () => response.json()
-  }
-}
 
 const {
   YOUTUBE_DL_PATH,
@@ -38,54 +28,27 @@ const getLatest = async data => {
   const { browser_download_url: url } = assets.find(
     ({ name }) => name === YOUTUBE_DL_FILE
   )
-  return get(url)
+  return httpGet(url)
 }
 
 const getBinary = async url => {
-  let response = await get(url)
+  let response = await httpGet(url)
   if (response.headers.get('content-type') !== 'application/octet-stream') {
     response = await getLatest(await response.json())
   }
-  return response.stream()
+  return Readable.fromWeb(response.body)
 }
 
 const installBinary = async () => {
-  log('Downloading `youtube-dl` binary')
-
-  const binary = await getBinary(YOUTUBE_DL_HOST)
-
-  const tmpfile = `${tmpdir()}/yt-dl-exec-${randomBytes(4).toString('hex')}`
-
-  log(`Download Location: \`${tmpfile}\``)
-
-  await pipeline(binary, createWriteStream(tmpfile))
-
-  log(`Ensuring directory exists: \`${YOUTUBE_DL_DIR}\``)
-
-  await mkdirp(YOUTUBE_DL_DIR)
-
-  log(`Moving binary to \`${YOUTUBE_DL_PATH}\``)
-
-  await rename(tmpfile, YOUTUBE_DL_PATH)
-
-  log(`Setting permissions for \`${YOUTUBE_DL_PATH}\``)
-
+  debug('downloading', { url: YOUTUBE_DL_HOST })
+  const [binary] = await Promise.all([
+    getBinary(YOUTUBE_DL_HOST),
+    mkdirp(YOUTUBE_DL_DIR)
+  ])
+  await pipeline(binary, createWriteStream(YOUTUBE_DL_PATH))
+  debug('writing', { path: YOUTUBE_DL_PATH })
   await chmod(YOUTUBE_DL_PATH, 0o755)
-
-  log('Installation complete')
+  debug({ status: 'success' })
 }
 
-const main = async () => {
-  if (YOUTUBE_DL_SKIP_DOWNLOAD) {
-    return log('Skipping `youtube-dl` binary download')
-  }
-
-  try {
-    await installBinary()
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
-  }
-}
-
-main()
+YOUTUBE_DL_SKIP_DOWNLOAD ? debug({ status: 'skipped' }) : installBinary()
